8f377d93eb4f01383a8ab1900f6789ac1be1b04e
Shiva1608 <112419070+Shiva1608@users.noreply.github.com>
2023-12-07 18:09:32 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 from app import app, datastore
-from model import db
+from model import db, Role
 
 
 with app.app_context():
     db.create_all()
-    datastore.find_or_create_role(name="admin", description="This user is an Admin")
-    datastore.find_or_create_role(name="manager", description="This user is a Store Manager")
-    datastore.find_or_create_role(name="customer", description="This user is a Customer")
+    datastore.find_or_create_role(name="admin", description="This user is an admin")
+    datastore.find_or_create_role(name="HOD", description="This user is the HOD")
     db.session.commit()
     datastore.create_user(username="Shiva", email="admin@gmail.com", password="12345", roles=["admin"])
-    datastore.create_user(username="Harshita", email="manager@gmail.com", password="123456", roles=["manager"])
-    datastore.create_user(username="Ana", email="customer@gmail.com", password="123456", roles=["customer"])
+    datastore.create_user(username="Harshita", email="hod@gmail.com", password="123456", roles=["HOD"])
     db.session.commit()
